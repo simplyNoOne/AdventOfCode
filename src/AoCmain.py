9edb0e0edc875fc9cd98253211e9ecd67d6bdf47
Ania000 <annasidor00@gmail.com>
2022-12-01 22:30:27 +0100

    Day1 done

--- src/AoCmain.py
+++ src/AoCmain.py
@@ -1,3 +1,7 @@
 from Day1 import Day1
 
 print("hello")
+
+day1 = Day1()
+day1.day1_base()
+day1.day1_extra()
--- src/Day1.py
+++ src/Day1.py
@@ -1,4 +1,46 @@
 class Day1:
 
-    def __init__(self):
-        return
+    def day1_base(self):
+        f = open("../inputs/day1.txt", "r")
+        elfs_cals = 0
+        max_cals = 0
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            if line != "\n":
+                elfs_cals += int(line)
+            else:
+                if elfs_cals > max_cals:
+                    max_cals = elfs_cals
+                elfs_cals = 0
+        print("Top elf's calories: " + str(max_cals))
+        f.close()
+
+    def day1_extra(self):
+        f = open("../inputs/day1.txt", "r")
+        elfs_cals = 0
+        max1_cals = 0
+        max2_cals = 0
+        max3_cals = 0
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            if line != "\n":
+                elfs_cals += int(line)
+            else:
+                if elfs_cals > max3_cals:
+                    if elfs_cals > max2_cals:
+                        max3_cals = max2_cals
+                        if elfs_cals > max1_cals:
+                            max2_cals = max1_cals
+                            max1_cals = elfs_cals
+                        else:
+                            max2_cals = elfs_cals
+                    else:
+                        max3_cals = elfs_cals
+                elfs_cals = 0
+
+        print("Calories from top 3 elves: " + str(max1_cals + max2_cals + max3_cals))
+        f.close()
