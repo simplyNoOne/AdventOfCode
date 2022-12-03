66552a858501032c8ba935b153ade427c3c90c3e
Ania000 <annasidor00@gmail.com>
2022-12-03 03:35:38 +0100

    Day2 im drunk dont jugde my code

--- src/AoCmain.py
+++ src/AoCmain.py
@@ -1,7 +1,12 @@
 from Day1 import Day1
+from Day2 import Day2
 
 print("hello")
 
-day1 = Day1()
-day1.day1_base()
-day1.day1_extra()
+#day1 = Day1()
+#day1.day1_base()
+#day1.day1_extra()
+
+day2 = Day2()
+day2.day2_base()
+day2.day2_extra()
